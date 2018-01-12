b5222ea206f33b9f27bdf95bcf40445b0e7fe05d
thesubway <thesubway@gmail.com>
2018-01-12 15:22:16 -0800

    Facebook to Firebase signin signout

--- TournamakeIos/ViewController.swift
+++ TournamakeIos/ViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var loginButton : FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.delegate = self
         loginButton.readPermissions = ["public_profile", "email"];
         loginButton.center = self.view.center;
         if let currentToken = FBSDKAccessToken.current() {
@@ -22,6 +26,34 @@ class ViewController: UIViewController {
             print("tokenString: \(currentToken.tokenString!)")
         }
         self.view.addSubview(loginButton);
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if let error = error {
+                // ...
+                return
+            }
+            // User is signed in
+            // ...
+            var userEmail = user!.email
+        }
+    
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            print("Firebase signout success")
+        } catch let signOutError as NSError {
+            print ("Firebase Error signing out: %@", signOutError)
+        }
     }
 
     override func didReceiveMemoryWarning() {
