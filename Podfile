341941253a0d76341949dd56b8e1d4075a7ae6cc
thesubway <thesubway@gmail.com>
2018-01-12 14:57:08 -0800

    Add GoogleService-Info, configure Firebase

--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 target 'TournamakeIos'
 pod 'Firebase'
 pod 'Firebase/Auth'
+pod 'Firebase/Core'
--- TournamakeIos/AppDelegate.swift
+++ TournamakeIos/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FBSDKCoreKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
