2a55a5bb80486df99588b729e3bbd295ab894cc8
thesubway <thesubway@gmail.com>
2018-01-12 13:40:47 -0800

    Initial Commit

---
+++ TournamakeIos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TournamakeIos
+//
+//  Created by Daniel Hoang on 1/12/18.
+//  Copyright © 2018 Daniel Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
